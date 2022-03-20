761a36ca6eeccf2934a4fecbf556b5fd33628fc0
sujeongHeo401 <gotnwjd40@gmail.com>
2022-03-20 22:17:09 +0900

    현재 REQUEST 를 받아올 수 있다 (HttpServletRequest  가져온다)

--- src/main/java/com/example/exception/controller/ApiController.java
+++ src/main/java/com/example/exception/controller/ApiController.java
@@ -1,19 +1,32 @@
 package com.example.exception.controller;
 
 import com.example.exception.dto.User;
+import org.hibernate.validator.internal.properties.Field;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @RequestMapping("") /// ? name = 1234
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+    public User get(
+            @Size(min = 2)
+            @RequestParam String name,
+            @NotNull
+            @Min(1)
+            @RequestParam(required = false) Integer age){
         User user = new User();
         user.setName(name);
         user.setAge(age);
@@ -30,7 +43,24 @@ public class ApiController {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
+
+        BindingResult bindingResult = e.getBindingResult();
+        bindingResult.getAllErrors().forEach(error -> {
+            FieldError field = (FieldError) error;
+            String fieldName = field.getField();
+            String message = field.getDefaultMessage();
+            String value = field.getRejectedValue().toString();
+
+
+            System.out.println("------------------");
+            System.out.println(fieldName);
+            System.out.println(message);
+            System.out.println(value);
+        });
         System.out.println("api controller");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
+
+
+//    public ResponseEntity ConstraintViolationException
 }
