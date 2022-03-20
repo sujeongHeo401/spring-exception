30474c4d59a4b1d4c350920968f124e4937b2fb7
sujeongHeo401 <gotnwjd40@gmail.com>
2022-03-20 19:57:31 +0900

    client 에서 친절하지 않았던 response 를  글로벌에서 우찌우찌 해줬지만은 무튼

--- src/main/java/com/example/exception/controller/ApiController.java
+++ src/main/java/com/example/exception/controller/ApiController.java
@@ -1,6 +1,9 @@
 package com.example.exception.controller;
 
 import com.example.exception.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,5 +26,11 @@ public class ApiController {
     public User post(@Valid @RequestBody User user){
         System.out.println(user);
         return user;
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
+        System.out.println("api controller");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
